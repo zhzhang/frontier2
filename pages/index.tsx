9835685ca4d281ac27a724103f2aa52f89f8766c
Jordan <zjzhang325@gmail.com>
2021-02-23 08:07:42 -0800

    Fix home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,27 @@
-import Layout from "../components/Layout";
-import Link from "next/link";
-import { withApollo } from "../lib/apollo";
-import gql from "graphql-tag";
-import { useQuery } from "@apollo/react-hooks";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Router from "next/router";
+import Row from "react-bootstrap/Row";
 
-const ArticleQuery = gql`
-  query ArticleQuery {
-    articles {
-      id
-    }
-  }
-`;
-
-const Blog = () => {
-  const { loading, error, data } = useQuery(ArticleQuery);
-  if (typeof window === "undefined") {
-    return <div>Loading ...</div>;
-  }
-  if (loading) {
-    return <div>Loading ...</div>;
-  }
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
-
+const Home = () => {
   return (
-    <Layout>
-      <div className="page">
-        <h1>My Blog</h1>
-        <main>
-          {data.articles.map((post) => (
-            <div key={post.id} className="post"></div>
-          ))}
-        </main>
-      </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
-
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
-
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
-    </Layout>
+    <Container style={{ paddingTop: 100 }} fluid>
+      <Row className="justify-content-md-center">
+        <h1>Welcome to Frontier.pub</h1>
+      </Row>
+      <Row className="justify-content-md-center">
+        <h4>Academic Review - Redesigned</h4>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Button
+          onClick={() => Router.push("/article/cklgwx4zu000058v2vz52cajm")}
+          variant="secondary"
+        >
+          View Demo
+        </Button>
+      </Row>
+    </Container>
   );
 };
 
-export default withApollo(Blog);
+export default Home;
