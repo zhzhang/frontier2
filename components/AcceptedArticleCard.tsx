bb632b4117c8b7cff2138b74582e15674b3b645f
Jordan <zjzhang325@gmail.com>
2021-02-24 14:35:07 -0800

    Deps

--- components/AcceptedArticleCard.tsx
+++ components/AcceptedArticleCard.tsx
@@ -5,22 +5,31 @@ import { Quill } from "./Quill";
 import { withApollo } from "../lib/apollo";
 import Review from "../components/Review";
 import { useState } from "react";
+import { ChevronUp, ChevronDown, PersonCircle } from "react-bootstrap-icons";
 import gql from "graphql-tag";
 
 const AcceptedArticleCard = ({ metaReview }) => {
   const { article } = metaReview;
   const { title, authors } = article;
+  const [open, setOpen] = useState(true);
   return (
-    <Accordion defaultActiveKey="1">
+    <Accordion activeKey={open ? "0" : null}>
       <Card style={{ padding: 10 }}>
         <h5>{title}</h5>
         {authors.map((author) => author.name)}
       </Card>
       <Card>
-        <Accordion.Toggle as={Card.Header} eventKey="1">
+        <Accordion.Toggle
+          as={Card.Header}
+          eventKey="0"
+          onClick={() => setOpen(!open)}
+        >
           {`Meta-Review by ${metaReview.author.name}`}
+          <span style={{ float: "right" }}>
+            {open ? <ChevronUp /> : <ChevronDown />}
+          </span>
         </Accordion.Toggle>
-        <Accordion.Collapse eventKey="1">
+        <Accordion.Collapse eventKey="0">
           <Card.Body>
             <Quill
               value={metaReview.body}
