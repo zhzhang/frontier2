7dc16bc346558deb9dcc4f0c779e7932aceff953
Jordan <zjzhang325@gmail.com>
2021-04-05 08:24:55 -0700

    Markdown side highlight detection

--- components/Markdown.tsx
+++ components/Markdown.tsx
@@ -6,29 +6,22 @@ import gfm from "remark-gfm";
 import visit from "unist-util-visit";
 import "katex/dist/katex.min.css"; // `react-katex` does not import the CSS for you
 
-const Highlight = ({ value, id, scrollTo }) => (
-  <div onClick={() => scrollTo(id)}>{value}</div>
-);
+const Highlight = ({ id, text, scrollTo }) => {
+  return <span onClick={() => scrollTo(id)}>{text}</span>;
+};
 
-const HIGHLIGHT_RE = /\[.*\]\{\d+\}/;
+const HIGHLIGHT_RE = /\[[^\]]+\]\{\d+\}/g;
 
-const extractText = (string, start, end) => {
-  const startLine = string.slice(0, start).split("\n");
-  const endLine = string.slice(0, end).split("\n");
+const extractParams = (string, start, end) => {
+  const matchedText = string.slice(start, end);
+  const parts = matchedText.split("]{");
+  const text = parts[0].slice(1, -1);
+  const id = parts[1].slice(0, -1);
 
   return {
-    type: "text",
-    value: string.slice(start, end),
-    position: {
-      start: {
-        line: startLine.length,
-        column: startLine[startLine.length - 1].length + 1,
-      },
-      end: {
-        line: endLine.length,
-        column: endLine[endLine.length - 1].length + 1,
-      },
-    },
+    type: "highlight",
+    text,
+    id,
   };
 };
 
@@ -40,26 +33,16 @@ const highlightPlugin = () => {
       let match;
 
       while ((match = HIGHLIGHT_RE.exec(node.value)) !== null) {
-        console.log(match);
         const value = match[0];
-        console.log(value);
         const type = "highlight";
 
         if (match.index !== lastIndex) {
-          definition.push(extractText(node.value, lastIndex, match.index));
+          definition.push(
+            extractParams(node.value, match.index, HIGHLIGHT_RE.lastIndex)
+          );
         }
 
-        definition.push({
-          type,
-          value,
-        });
-
         lastIndex = match.index + value.length;
-      }
-
-      if (lastIndex !== node.value.length) {
-        const text = extractText(node.value, lastIndex, node.value.length);
-        definition.push(text);
       }
 
       const last = parent.children.slice(position + 1);
@@ -76,8 +59,8 @@ const Markdown = ({ highlights, scrollTo, children }) => {
   const renderers = {
     inlineMath: ({ value }) => <InlineMath math={value} />,
     math: ({ value }) => <BlockMath math={value} />,
-    highlight: ({ value, id }) => (
-      <Highlight value={value} id={id} scrollTo={scrollTo} />
+    highlight: ({ text, id }) => (
+      <Highlight text={text} id={id} scrollTo={scrollTo} />
     ),
   };
   return (
