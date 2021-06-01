4715d833451cf1bb7deeaaaf756c63c27258879d
Jordan <zjzhang325@gmail.com>
2021-06-01 15:05:58 -0700

    Save config changes

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -1,9 +1,9 @@
-import Jumbotron from "react-bootstrap/Jumbotron";
+import Card from "react-bootstrap/Card";
 import Router from "next/router";
 import Markdown from "./Markdown";
-import { withApollo } from "../lib/apollo";
-import OrganizationBadge from "../components/OrganizationBadge";
-import UserBadge from "../components/UserBadge";
+import { withApollo } from "@/lib/apollo";
+import OrganizationBadge from "@/components/OrganizationBadge";
+import UserBadge from "@/components/UserBadge";
 import { useState } from "react";
 
 const ArticleCard = ({ article }) => {
@@ -11,31 +11,33 @@ const ArticleCard = ({ article }) => {
   const abstract = versions[0].abstract;
   const [hover, setHover] = useState(false);
   return (
-    <Jumbotron>
-      <h4
-        onClick={() => Router.push(`/article/${id}`)}
-        onMouseEnter={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        style={{ color: hover ? "blue" : "black" }}
-      >
-        {title}
-      </h4>
-      <span>
-        Authors:{" "}
-        {authors !== null ? (
-          authors.map((author) => <UserBadge user={author} />)
-        ) : (
-          <em>anonymized</em>
-        )}
-      </span>
-      <Markdown>{abstract}</Markdown>
-      <span>
-        {acceptedOrganizations.length === 0 ? null : "Accepted by: "}
-        {acceptedOrganizations.map((org) => (
-          <OrganizationBadge organization={org} />
-        ))}
-      </span>
-    </Jumbotron>
+    <Card>
+      <Card.Body>
+        <h5
+          onClick={() => Router.push(`/article/${id}`)}
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+          style={{ color: hover ? "blue" : "black" }}
+        >
+          {title}
+        </h5>
+        <div>
+          Authors:{" "}
+          {authors !== null ? (
+            authors.map((author) => <UserBadge user={author} />)
+          ) : (
+            <em>anonymized</em>
+          )}
+        </div>
+        <div>
+          {acceptedOrganizations.length === 0 ? null : "Accepted by: "}
+          {acceptedOrganizations.map((org) => (
+            <OrganizationBadge organization={org} />
+          ))}
+        </div>
+        <Markdown>{abstract}</Markdown>
+      </Card.Body>
+    </Card>
   );
 };
 
---
+++ prisma/migrations/20210527045554_/migration.sql
@@ -0,0 +1,2 @@
+-- AlterIndex
+ALTER TABLE `Submission` RENAME INDEX `Submission_decisionId_unique` TO `Submission.decisionId_unique`;
